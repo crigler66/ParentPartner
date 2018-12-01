c8ccd9fd44056b8d90cff2e236162bdccf53056b
Keith Crigler <big66kc@yahoo.com>
2018-12-01 06:08:31 -0700

    4 iteration home page only

--- app/src/main/java/com/dna/parentpartner/Home.java
+++ app/src/main/java/com/dna/parentpartner/Home.java
@@ -80,17 +80,13 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_activites) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_rewards) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_progress) {
 
         }
 
