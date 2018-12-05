f96f687411819ef693d70d5e8c0c8231d2b29919
Keith Crigler <big66kc@yahoo.com>
2018-12-05 13:35:26 -0700

    4 iteration home page only

---
+++ app/src/main/java/com/dna/parentpartner/Rewards.java
@@ -0,0 +1,13 @@
+package com.dna.parentpartner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rewards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewards);
+    }
+}
